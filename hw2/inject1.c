1bda1948c2b99474ec808420e44b8528ac2d307f
JubeatWwW <l6104400@gmail.com>
2017-04-16 17:07:35 +0800

    add new function

--- hw2/inject1.c
+++ hw2/inject1.c
@@ -2,33 +2,94 @@
 #include <stdlib.h>
 #include <unistd.h>
 #include <dlfcn.h>
+#include <dirent.h>
+#include <sys/types.h>
 
-#define DECLARE(TYPE, NAME, ARG) static TYPE (*old_##NAME)(ARG) = NULL;
+#define DECLARE(TYPE, NAME, ...) static TYPE (*old_##NAME)(__VA_ARGS) = NULL;
 #define DLSYM(NAME) old_##NAME = dlsym(handle, #NAME);
+#define RETURN(NAME) return old_##NAME(arg);
 
-DECLARE(uid_t, getuid, void);
-DECLARE(char*, getenv, const char*);
+DECLARE(char*,          getenv,     const char*);
+
+DECLARE(int,            closedir,   DIR*);
+DECLARE(DIR*,           fdopendir,  int);
+DECLARE(DIR*,           opendir,    const char*);
+DECLARE(struct dirent*, readdir,    DIR*)
+DECLARE(int,            readdir_r,  DIR*, struct dirent*, struct dirent*);
+DECLARE(void,           rewinddir,  DIR*);
+DECLARE(void,           seekdir,    DIR*, long);
+DECLARE(long,           telldir,    DIR*)
+
+DECLARE(int,            creat,      const char*, mode_t);
+DECLARE(int,            open,       const char*, int);
+DECLARE(int,            remove,     const char*);
+DECLARE(int,            rename,     const char*, const char*);
+DECLARE(void,           setbuf,     FILE*, char*);
+DECLARE(int,            setvbuf,    FILE*, char*, int, size_t);
+DECLARE(char*,          tempnam,    const char*, const char*);
+DECLARE(FILE*,          tmpfile,    void);
 
 __attribute__((constructor)) void HijackInit() {
     void *handle = dlopen("libc.so.6", RTLD_LAZY);
 
     if(NULL != handle){
+        DLSYM(closedir);
+        DLSYM(fdopendir);
+        DLSYM(opendir);
+        DLSYM(readdir);
+        DLSYM(readdir_r);
+        DLSYM(rewinddir);
+        DLSYM(seekdir);
+        DLSYM(telldir);
+
         DLSYM(getenv);
-        DLSYM(getuid);
     }
 }
 
-char* getenv(const char* name){
+char* getenv(const char* arg){
 
-    fprintf(stderr, "getenv injected\n");
-
-    if(old_getenv != NULL)
-        fprintf(stderr, "%s\n", old_getenv(name));
-    return old_getenv(name) ;
+    fprintf(stderr, "[monitor] getenv('%s') = '%s'\n", arg, old_getenv(arg));
+    RETURN(getenv);
 }
 
-uid_t getuid(void){
-    fprintf(stderr, "RRRRRRRRRRRRRRRRRRRRRRRR\n");
+int closedir(DIR* arg){
+    fprintf(stderr, "closedir injected\n");
+    
+    RETURN(closedir);
+}
 
-    return old_getuid();
+DIR* fdopendir(int arg){
+    fprintf(stderr, "fdopendir injected\n");
+    
+    RETURN(fdopendir);
+}
+
+DIR* opendir(const char* arg){
+    fprintf(stderr, "opendir YAYAYA\n");
+    RETURN(opendir);
+}
+
+struct dirent* readdir(DIR* arg){
+    fprintf(stderr, "readdir fuck u\n");
+    RETURN(readdir);
+}
+
+int readdir_r(DIR *dirp, struct dirent *entry, struct dirent **result){
+    fprintf(stderr, "readdir\n");
+    return old_readdir_r(dirp, entry, result);
+}
+
+void rewinddir(DIR *dirp){
+    fprintf(stderr, "[monitor] rewinddir\n");
+    old_rewinddir(dirp);
+}
+
+void seekdir(DIR* dirp, long doc){
+    fprintf(stderr, "[monitor] seekdir\n");
+    old_seekdir(dirp, doc);
+}
+
+long telldir(DIR* arg){
+    fprintf(stderr, "[monitor] telldir = %ld", old_telldir(arg));
+    RETURN(telldir);
 }
